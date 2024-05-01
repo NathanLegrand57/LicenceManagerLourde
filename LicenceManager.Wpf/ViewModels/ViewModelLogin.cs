fc87be18432f1fb2c1fb1282f683a7b6ecc10daf
Nathan <nathanlgd14@gmail.com>
2024-05-01 22:08:06 +0200

    Vérification du rôle de l'utilisateur lors de l'authentification (Si l'utilisateur n'est pas Admin ou Employé, il ne peut pas se connecter)

--- LicenceManager.Wpf/ViewModels/ViewModelLogin.cs
+++ LicenceManager.Wpf/ViewModels/ViewModelLogin.cs
@@ -1,5 +1,7 @@
 ﻿using LicenceManager.Core;
 using LicenceManager.DBLib.Class;
+using System.Data;
+
 //using Microsoft.AspNet.Identity;
 using System.Windows.Forms;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
@@ -25,26 +27,43 @@ namespace LicenceManager.Wpf.ViewModels
         public void Login()
         {
             User? user = null;
+            Role? role = null;
+            bool isEmploye = false;
+            bool isAdmin = false;
             using (LicencemanagerContext context = new())
-                user = context.Users.FirstOrDefault(userTemp => userTemp.Libelle.Equals(Libelle));
-
-            if (user != null)
             {
-                // Vérifier le mot de passe haché avec BCrypt
-                if (BCrypt.Net.BCrypt.Verify(Password, user.Password))
+                user = context.Users.FirstOrDefault(userTemp => userTemp.Libelle.Equals(Libelle));
+                Role employeRole = context.Roles.First(r => r.Name == "employe");
+                Role adminRole = context.Roles.First(r => r.Name == "admin");
+
+                if (user != null)
                 {
-                    ((App)App.Current).Login(user);
+                    isEmploye = context.AssignedRoles.Any(ar => ar.EntityId == user.Id && ar.RoleId == employeRole.Id);
+                    isAdmin = context.AssignedRoles.Any(ar => ar.EntityId == user.Id && ar.RoleId == adminRole.Id);
+                    if (isEmploye || isAdmin )
+                    {
+                        // Vérifier le mot de passe haché avec BCrypt
+                        if (BCrypt.Net.BCrypt.Verify(Password, user.Password))
+                        {
+                            ((App)App.Current).Login(user);
+                        }
+                        else
+                        {
+                            // Mot de passe incorrect
+                            MessageBox.Show("Mot de passe incorrect", "Erreur de connexion", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    }
+                    else
+                    {
+                        // L'utilisateur n'a pas les autorisations nécessaires
+                        MessageBox.Show("Vous n'avez pas les autorisations nécessaires pour accéder à cette application", "Erreur de connexion", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
                 }
                 else
                 {
-                    // Mot de passe incorrect
-                    MessageBox.Show("Mot de passe incorrect", "Erreur de connexion", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    // Nom d'utilisateur introuvable
+                    MessageBox.Show("Nom d'utilisateur introuvable", "Erreur de connexion", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
-            }
-            else
-            {
-                // Nom d'utilisateur introuvable
-                MessageBox.Show("Nom d'utilisateur introuvable", "Erreur de connexion", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
     }
